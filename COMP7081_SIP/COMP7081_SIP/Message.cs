a968a672a261d408060a16863f501bed2d9ee2b4
blah <blah@blah.com>
2013-12-05 01:22:25 -0800

    registration adds to csv file succesfully

--- COMP7081_SIP/COMP7081_SIP/Message.cs
+++ COMP7081_SIP/COMP7081_SIP/Message.cs
@@ -14,19 +14,28 @@ namespace COMP7081_SIP
         public String toIP { get; private set; }
         public String user { get; private set; }
         public String ip { get; private set; }
+        public String branch { get; private set; }
 
         public Message(String receivedString)
         {
             String[] messageParts = receivedString.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
             String[] contact = messageParts[(int)headerFieldIndex.Contact].Split(new string[] { ":", "<", "@", ">", ";" }, StringSplitOptions.RemoveEmptyEntries);
             String[] to = messageParts[(int)headerFieldIndex.To].Split(new string[] { "<", ":", "@", ">" }, StringSplitOptions.RemoveEmptyEntries);
+            String[] via = messageParts[(int)headerFieldIndex.Via].Split(new string[] { ";", "=" }, StringSplitOptions.RemoveEmptyEntries);
 
             type = messageParts[(int) headerFieldIndex.Type].Split(' ')[0];
             user = contact[3];
             ip = contact[4];
-
+            branch = via[3];
             toUser = to[3];
             toIP = to[4];
+        }
+
+        public string registrationSuccessMessage()
+        {
+            //String response = "SIP/2.0 200 Registration successful\r\n"
+              //              + 
+            return "";
         }
     }
 }
