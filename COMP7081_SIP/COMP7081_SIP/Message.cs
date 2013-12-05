c07cf52854d0115ee0d451610c464f2472687d60
blah <blah@blah.com>
2013-12-05 00:23:18 -0800

    can get user, ip and destination from messages now

--- COMP7081_SIP/COMP7081_SIP/Message.cs
+++ COMP7081_SIP/COMP7081_SIP/Message.cs
@@ -7,19 +7,26 @@ namespace COMP7081_SIP
 {
     class Message
     {
-        String invite;
-        String via;
-        String to;
-        String from;
-        String callid;
-        String cseq;
-        String contact;
-        String contactType;
-        String contentLength;
+        enum headerFieldIndex { Type, Via, From, To, CallID, CSeq, Contact, MaxForwards, UserAgent, Expires, ContentLength };
 
-        public Message(byte[] bytes)
+        public String type { get; private set; }
+        public String toUser { get; private set; }
+        public String toIP { get; private set; }
+        public String user { get; private set; }
+        public String ip { get; private set; }
+
+        public Message(String receivedString)
         {
-            System.Console.Out.WriteLine("Hello");
+            String[] messageParts = receivedString.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            String[] contact = messageParts[(int)headerFieldIndex.Contact].Split(new string[] { ":", "<", "@", ">", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            String[] to = messageParts[(int)headerFieldIndex.To].Split(new string[] { "<", ":", "@", ">" }, StringSplitOptions.RemoveEmptyEntries);
+
+            type = messageParts[(int) headerFieldIndex.Type].Split(' ')[0];
+            user = contact[3];
+            ip = contact[4];
+
+            toUser = to[3];
+            toIP = to[4];
         }
     }
 }
--- COMP7081_SIP/COMP7081_SIP/SIPProxyServer.cs
+++ COMP7081_SIP/COMP7081_SIP/SIPProxyServer.cs
@@ -9,7 +9,7 @@ namespace COMP7081_SIP
 {
     class SIPProxyServer
     {
-        private const int PORTNUM = 8080;
+        private const int PORTNUM = 5060;
 
         protected UdpClient udpClient;
         private IPEndPoint ipEndPoint;
@@ -22,19 +22,46 @@ namespace COMP7081_SIP
             host = Dns.GetHostEntry(Dns.GetHostName());
             // Create the socket we'll listen on.
             ipEndPoint = new IPEndPoint(IPAddress.Any, PORTNUM);
-            System.Console.WriteLine("Constructor done");
         }
 
         public void run()
         {
-            byte[] received_bytes;
+            byte[] receivedBytes; // Used to store the bytestream we get from the socket.
+            Message receivedMessage;
 
             while (true)
             {
-                System.Console.WriteLine("HELLO");
-                received_bytes = udpClient.Receive(ref ipEndPoint);
-                System.Console.WriteLine("Recieved from: " + ipEndPoint);
-                System.Console.WriteLine(Encoding.ASCII.GetString(received_bytes));
+                // Block on receive until we get a message.
+                receivedBytes = udpClient.Receive(ref ipEndPoint);
+
+                String receivedString = Encoding.ASCII.GetString(receivedBytes); // Convert the bytes to a string.
+                
+                if (!receivedString.Equals("jaK\0"))
+                {
+                    receivedMessage = new Message(receivedString);
+
+                    // If the message is a REGISTER message, try to register the user.
+                    if (receivedMessage.Equals("REGISTER"))
+                    {
+                        // Check if the user is already registered. If so, replace it. If not, add.
+
+                    }
+                    // Otherwise, check if the user is registered and forward it.
+                    else
+                    {
+                        // Check if the user is registered before forwarding.
+                        //if ()
+                        {
+                            udpClient.Send(receivedBytes, receivedBytes.Length, receivedMessage.toIP, PORTNUM);
+                        }
+                    }
+
+                    System.Console.WriteLine("Type:" + receivedMessage.type);
+                    System.Console.WriteLine("Type:" + receivedMessage.user);
+                    System.Console.WriteLine("Type:" + receivedMessage.ip);
+                    System.Console.WriteLine("Type:" + receivedMessage.toIP);
+                    System.Console.WriteLine("Type:" + receivedMessage.toUser);
+                }
             }
         }
     }
