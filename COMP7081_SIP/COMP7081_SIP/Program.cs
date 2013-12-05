3413bd7bdcbf573399c14f0fe84f03be7f64492c
rofl <lol>
2013-12-04 21:42:25 -0800

    'LOL'

---
+++ COMP7081_SIP/COMP7081_SIP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP7081_SIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
